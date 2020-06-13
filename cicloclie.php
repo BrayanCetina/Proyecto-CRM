b2b867c543415a5c4e5adaa9a40d4e539fb05739
Christian100298 <43640517+Christian100298@users.noreply.github.com>
2020-06-13 10:29:22 -0500

    diseño
    pesa

--- cicloclie.php
+++ cicloclie.php
@@ -88,7 +88,7 @@ $dataPoints = array(
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
+          <i class="fas fa-dumbbell"></i>
         </div>
         <div class="sidebar-brand-text mx-3">CRM GYM<sup></sup></div>
       </a>
